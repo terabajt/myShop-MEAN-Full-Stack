125935cb73e3fb631256596ab631eda9d4360378
Michał Pasynek <terabajt@terabajtlw.pl>
2023-10-02 19:30:16 +0200

    [FEATURE - backend] Added id instead of _id to the category API. Disabled JWT for testing.

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -9,12 +9,13 @@ function authJwt() {
 		isRevoked: isRevoked,
 	}).unless({
 		path: [
-			{ url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
-			{ url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
-			{ url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
-			{ url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS'] },
-			`${api}/users/login`,
-			`${api}/users/register`,
+			// { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
+			// { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+			// { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+			// { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS'] },
+			// `${api}/users/login`,
+			// `${api}/users/register`,
+			{ url: /(.*)/ },
 		],
 	});
 }
